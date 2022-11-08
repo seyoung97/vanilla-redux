08993286dfbe484c338890ca53331120836f95f4
Joey <seyoung1st@gmail.com>
2022-11-08 14:38:16 +0900

    Refactor: if 문 switch로 변경, action type 변수에 저장

--- src/index.js
+++ src/index.js
@@ -5,15 +5,20 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 number.innerText = 0;
+//action의 타입을 스트링 말고 변수에 담아서 사용하면 오타 같은 실수를 방지할 수 있음
+const ADD = "ADD";
+const MINUS = "MINUS";
 
 const countModifier = (count = 0, action) => {
   console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  //if문 보다 switch 문 쓰는게 훨씬 깔끔 간결
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -25,8 +30,8 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
-add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
 minus.addEventListener("click", handleMinus);
